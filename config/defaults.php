3577a031e29278520374e4224f43b16c24daca1d
ADmad <admad.coder@gmail.com>
2015-07-04 12:40:33 +0530

    Use CDN.

--- config/defaults.php
+++ config/defaults.php
@@ -4,15 +4,15 @@ return [
     'CrudView' => [
         'css' => [
             'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.css',
-            'CrudView./eonasdan-bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.14.30/css/bootstrap-datetimepicker.min.css',
             'CrudView.local'
         ],
         'js' => [
             'headjs' => [
                 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js',
-                'CrudView./moment/moment-with-locales.min.js',
-                'CrudView./eonasdan-bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment-with-locales.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.14.30/js/bootstrap-datetimepicker.min.js',
             ],
             'script' => [
                 'CrudView.local'
