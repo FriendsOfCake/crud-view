c25b5f690d6b5538479574ff001f3ba8f61c201f
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-12-05 05:59:48 -0700

    docs: fix issue in setup return

--- docs/_config/cakephpbranch.py
+++ docs/_config/cakephpbranch.py
@@ -9,7 +9,6 @@ the GitHub branch name of the docs' version.
 def setup(app):
     app.connect('html-page-context', append_template_ctx)
     app.add_config_value('branch', '', True)
-    return app
 
 
 def append_template_ctx(app, pagename, templatename, ctx, event_arg):
