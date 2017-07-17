c347c89db2538d5db03de8473485a1a512cb6c11
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2017-07-17 01:19:52 -0600

    chore: add trailing commas

--- config/defaults.php
+++ config/defaults.php
@@ -8,7 +8,7 @@ return [
             'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.css',
             'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css',
             'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.2/css/selectize.bootstrap3.min.css',
-            'CrudView.local'
+            'CrudView.local',
         ],
         'js' => [
             'headjs' => [
@@ -17,16 +17,16 @@ return [
                 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.2/js/standalone/selectize.min.js',
-                'https://cdn.jsdelivr.net/jquery.dirtyforms/1.2.2/jquery.dirtyforms.min.js'
+                'https://cdn.jsdelivr.net/jquery.dirtyforms/1.2.2/jquery.dirtyforms.min.js',
             ],
             'script' => [
                 'CrudView.local'
-            ]
+            ],
         ],
         'timezoneAwareDateTimeWidget' => false,
         'useAssetCompress' => Plugin::loaded('AssetCompress'),
         'tablesBlacklist' => [
-            'phinxlog'
-        ]
+            'phinxlog',
+        ],
     ]
 ];
