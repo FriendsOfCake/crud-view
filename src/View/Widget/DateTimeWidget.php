051a8432918bffb79ad1759637d45368c5fb0a6f
ADmad <admad.coder@gmail.com>
2019-09-30 03:07:48 +0530

    Move back picker icon to the right

--- src/View/Widget/DateTimeWidget.php
+++ src/View/Widget/DateTimeWidget.php
@@ -88,10 +88,10 @@ class DateTimeWidget extends \BootstrapUI\View\Widget\DateTimeWidget
             $this->_templates->add([
                 'datetimePicker' =>
                     '<div {{attrs}}>'
-                    . '<div class="input-group-prepend">'
+                    . '{{input}}'
+                    . '<div class="input-group-append">'
                     . '<span data-toggle class="input-group-text"><i class="fa fa-calendar-alt"></i></span>'
                     . '</div>'
-                    . '{{input}}'
                     . '</div>',
             ]);
         }
