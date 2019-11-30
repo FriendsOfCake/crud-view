a124770b35df52f1b83b195d4dd0a8e1d71d2d8f
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-11-29 23:00:13 -0500

    feat: Create Plugin class

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+namespace CrudView;
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class for CrudView
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'CrudView';
+}
