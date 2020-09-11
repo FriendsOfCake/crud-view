c9387417ad074f2204638a8213afeab440eace8d
ADmad <admad.coder@gmail.com>
2020-09-11 18:31:47 +0530

    Update config files

--- config/paginator.php
+++ config/paginator.php
@@ -1,5 +1,5 @@
 <?php
-$config = [
+return [
     'nextActive' => '<li class="next active"><a rel="next" href="{{url}}">{{text}}</a></li>',
     'prevActive' => '<li class="prev active"><a rel="prev" href="{{url}}">{{text}}</a></li>',
 ];
