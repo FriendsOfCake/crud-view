1d7a708fe434be30915cdff8d35805acb871370f
ADmad <admad.coder@gmail.com>
2024-03-28 18:53:02 +0530

    Fix tests

--- tests/Fixture/BlogsFixture.php
+++ tests/Fixture/BlogsFixture.php
@@ -7,16 +7,7 @@ use Cake\TestSuite\Fixture\TestFixture;
 
 class BlogsFixture extends TestFixture
 {
-    public $fields = [
-        'id' => ['type' => 'integer'],
-        'is_active' => ['type' => 'boolean', 'default' => true, 'null' => false],
-        'name' => ['type' => 'string', 'length' => 255, 'null' => false],
-        'body' => ['type' => 'text', 'null' => false],
-        'user_id' => ['type' => 'integer'],
-        '_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]],
-    ];
-
-    public $records = [
+    public array $records = [
         ['name' => '1st post', 'body' => '1st post body', 'user_id' => 1],
         ['name' => '2nd post', 'body' => '2nd post body', 'user_id' => 1],
         ['name' => '3rd post', 'body' => '3rd post body', 'user_id' => 1],
