94c581221110aa7f586e17ee4b878c0b174409d2
ADmad <admad.coder@gmail.com>
2024-09-17 18:22:02 +0530

    disable unneeded plugin hooks

--- src/CrudViewPlugin.php
+++ src/CrudViewPlugin.php
@@ -16,6 +16,7 @@ class CrudViewPlugin extends BasePlugin
      * @var string|null
      */
     protected ?string $name = 'CrudView';
+
     /**
      * Do bootstrapping or not
      *
@@ -31,9 +32,30 @@ class CrudViewPlugin extends BasePlugin
     protected bool $consoleEnabled = false;
 
     /**
+     * Enable middleware
+     *
+     * @var bool
+     */
+    protected bool $middlewareEnabled = false;
+
+    /**
+     * Register container services
+     *
+     * @var bool
+     */
+    protected bool $servicesEnabled = false;
+
+    /**
      * Load routes or not
      *
      * @var bool
      */
     protected bool $routesEnabled = false;
+
+    /**
+     * Load events or not
+     *
+     * @var bool
+     */
+    protected bool $eventsEnabled = false;
 }
